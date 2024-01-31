082da922b22ca08f852678d48f766cb7cfddc2e0
mkizhevsk <mkizhevsk@gmail.com>
2024-01-31 16:37:51 +0400

    first commit

---
+++ src/test/java/com/mk/myspaceweb/MySpaceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.myspaceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpaceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
