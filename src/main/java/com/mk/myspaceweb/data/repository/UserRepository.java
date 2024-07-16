df87ef9f835a7d5cb506a803c5a45df55808a096
mkizhevsk <mkizhevsk@gmail.com>
2024-07-16 18:03:15 +0400

    User and Role added

---
+++ src/main/java/com/mk/myspaceweb/data/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mk.myspaceweb.data.repository;
+
+import com.mk.myspaceweb.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
